c1f6a9a3a35bfcc2b56c37e71af40dff773c6afa
Yippie_Calle <cal12001@byui.edu>
2021-12-13 23:13:19 -0700

    Added a good amount of items
    Updated the nav bar, footer, created an about page.
    
    need to work on the navbar alittle bit more fix the styling

--- SacramentPlanner/SacramentPlanner/Controllers/HomeController.cs
+++ SacramentPlanner/SacramentPlanner/Controllers/HomeController.cs
@@ -27,7 +27,10 @@ namespace SacramentPlanner.Controllers
         {
             return View();
         }
-
+        public IActionResult About()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
