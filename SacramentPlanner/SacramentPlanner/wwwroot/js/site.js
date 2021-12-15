894ac989565c96524a69bca568652b4fe3831599
Yippie_Calle <cal12001@byui.edu>
2021-12-15 14:22:02 -0700

    fixed a few things and added buttons

--- SacramentPlanner/SacramentPlanner/wwwroot/js/site.js
+++ SacramentPlanner/SacramentPlanner/wwwroot/js/site.js
@@ -3,17 +3,18 @@
 
 // Write your JavaScript code.
 $(document).ready(function () {
+    $("#myBtn3").click(function () {
+        $("#myModal3").modal();
+    });
+    $("#myBtn2").click(function () {
+        $("#myModal2").modal();
+    });
     $("#myBtn1").click(function () {
         $("#myModal1").modal();
     });
 });
-$(document).ready(function () {
-    $("#myBtn2").click(function () {
-        $("#myModal2").modal();
-    });
-});
-$(document).ready(function () {
-    $("#myBtn3").click(function () {
-        $("#myModal3").modal();
-    });
+$('#multi1').mdbRange({
+    single: {
+        active: true
+    }
 });
